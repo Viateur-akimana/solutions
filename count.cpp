a318d8c29463db2fa229205d75119332079d487f
Viateur <akimanaviateur94@gmail.com>
2023-11-04 10:44:28 +0100

    character counts

---
+++ count.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+    string sentence = "akimana viateur isstudying c++";
+    //number of character wth out spaces
+    int numberOfCharacterWithOutSpaces = 0;
+    for(char c: sentence){
+        if(c != ' '){
+            numberOfCharacterWithOutSpaces++;
+        }
+    }
+    //number of characters with spaces
+    int numberOfCharactersWithSpaces = sentence.length();
+    //number of words
+cout << numberOfCharactersWithSpaces <<numberOfCharacterWithOutSpaces <<endl;
+
+    return 0;
+}
